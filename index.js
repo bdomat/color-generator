5a216f8e384df98f6b5f5c6cfb7d9f8f59b75b32
Bertrand Domat <bertrand.domat@gmail.com>
2023-01-26 16:38:18 +0100

    Finalisation du projet

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //Création des 3 variables
 
-let red = Math.floor(Math.random() * 255);
-let green = Math.floor(Math.random() * 255);
-let blue = Math.floor(Math.random() * 255);
-const colorValue = document.querySelector(".color-value");
-
 //donner couleur de fond au body
 
 //insérer couleur rgb dans body
 
-const colorGenerator = () => {
-  document.body.style.background = `rgb(${red}, ${green}, ${blue})`;
-  colorValue.textContent = `rgb(${red}, ${green}, ${blue})`;
-};
-setTimeout(colorGenerator, 10);
+function colorGenerator() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let colorValue = document.querySelector(".color-value");
+  let rgb = `rgb(${red}, ${green}, ${blue})`;
+  document.body.style.background = rgb;
+  colorValue.textContent = rgb;
+}
+setInterval(colorGenerator, 2000);
